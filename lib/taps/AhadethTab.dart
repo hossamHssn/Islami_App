170e85402819392134cee5d807775edbfbb540d0
hossam <hossamhassnh22@gmail.com>
2023-07-16 15:27:15 +0300

    add hadeth file txt

--- lib/taps/AhadethTab.dart
+++ lib/taps/AhadethTab.dart
@@ -5,8 +5,28 @@ class AhadethTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(
+            "assets/images/hadeth_logo.png",
+          ),
+          Divider(
+            thickness: 2,
+            color: Theme.of(context).primaryColor,
+          ),
+          Text(
+            "الاحاديث",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            textAlign: TextAlign.center,
+          ),
+          Divider(
+            thickness: 2,
+            color: Theme.of(context).primaryColor,
+          ),
+        ],
+      ),
     );
   }
 }
